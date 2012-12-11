c39a07e95a57be2a2a7e791df4c1cb3b78baca62
hysios hu <hysios@gmail.com>
2012-12-11 15:43:03 +0800

    setup startup files widget.rb

--- lib/widget_ui.rb
+++ lib/widget_ui.rb
@@ -3,3 +3,7 @@ require "widget_ui/version"
 module WidgetUI
   # Your code goes here...
 end
+
+require 'base_widget'
+require 'dsl'
+require 'controller_methods'
--- lib/widget_ui/widget_ui.rb
+++
@@ -1,7 +0,0 @@
-module WidgetUI
-
-end
-
-require 'base_widget'
-require 'dsl'
-require 'controller_methods'
