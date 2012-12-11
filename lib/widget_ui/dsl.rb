aef6892b7026146f2b9274e7dc18d7f6fcca088c
hysios hu <hysios@gmail.com>
2012-12-11 15:51:19 +0800

    fix dsl load file error

--- lib/widget_ui/dsl.rb
+++ lib/widget_ui/dsl.rb
@@ -3,6 +3,6 @@ module WidgetUI
   end
 end
 
-dsl_path = File.expand_path('../dsl/', __FILE__)
-require dsl_path + 'builder'
-require dsl_path + 'controls'
+dsl_path = File.dirname __FILE__
+require File.join dsl_path, 'dsl/builder'
+require File.join dsl_path, 'dsl/controls'
