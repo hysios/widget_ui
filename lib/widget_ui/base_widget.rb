74f7825631d996a4e7cf0b48cc01ea965c7da74f
hysios hu <hysios@gmail.com>
2012-12-13 11:12:19 +0800

    Revert "move method event_process to base_widget.rb"
    This reverts commit f0008c9ef8d453c3e803c88286e5e858f19a36fd.

--- lib/widget_ui/base_widget.rb
+++ lib/widget_ui/base_widget.rb
@@ -4,12 +4,5 @@ module WidgetUI
   class UIBase < Apotomo::Widget
     include WidgetUI::EventHook
 
-    def events_process(evt)
-      event, callback = self.class.events_callback.find do |key, value| 
-        evt.type == key
-      end
-
-      self.instance_exec &callback if callback.is_a? Proc
-    end
   end
 end
